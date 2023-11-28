7b5f3e3d187db12a4dc7dcc027c673d375418397
ThaissaMariana <thaissa2608mariana@gmail.com>
2023-11-28 07:54:52 -0400

    Commit está dando tudo errado

--- src/Componentes/Menu.jsx
+++ src/Componentes/Menu.jsx
@@ -2,16 +2,21 @@ import { Link } from "react-router-dom"
 import "../style/card.css"
 
 function Cards({ listaVideos }) {
+
     return (
-        listaVideos.map((video) =>  
+        
+        listaVideos.map((video) =>
          <div className="card">
             <iframe
-             width="500"
-             height="500"
+            width="500"
+            height="500"
+            scr={ 
+                "https://www.youtube.com/embed/" + video.url.slice(10)}
+                frameborder="0"
+                allow="accelerometer; autoplay; clipboard-white; encrypted-media;">
+                </iframe>
 
-            </div>
-        
-        
+         </div>
         )
     )
 }
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,7 @@
 import {Link} from "react-router-dom";
 import Header from "./Componentes/Header";
 export default function Home(){
-    return(
-        <div>
-            <Header/>
-
+    return (
         <h1>PTAC 2 - PROJETO FINAL</h1>
-        </div>
-     );
+    );
 }
